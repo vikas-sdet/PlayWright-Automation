5aaf47f23a19f1aa62d0f1a85f66c946ceb7e959
Vikas Kumar <163975383+vikucodes@users.noreply.github.com>
2026-02-06 11:24:32 +0530

    Update Add1.spec.js

---
+++ tests/Add1.spec.js
@@ -0,0 +1,27 @@
+const {test ,expect} =require ('@playwright/test')
+
+test ("Popup validations" ,async({page})=>
+{
+
+await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+// await page.goto("http://google.com");
+// await page.goBack();
+// await page.goForward();
+
+//validate hidden and visible
+
+await expect(page.locator("#displayed-text")).toBeVisible();
+await page.locator("#hide-textbox").click();
+await expect(page.locator("#displayed-text")).toBeHidden();
+await page.pause();
+
+// handle dialog or popup
+page.on('dialog' , dialog => dialog.accept());
+await page.locator("#confirmbtn").click();
+
+//mouse Hover
+
+await page.locator("#mousehover").hover();
+
+}
+)
